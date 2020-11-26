a4c73609402aafe3ba01167dbbe75ad8597a9724
JJB <whwjdqja20@naver.com>
2020-11-26 11:50:12 +0900

    modified GalleryFragment.kt(adapter.addloadStateListener, buttonRertry.setOnClickListener)

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -5,9 +5,11 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.paging.LoadState
 
 import com.codinginflow.imagesearchapp.R
 import com.codinginflow.imagesearchapp.databinding.FragmentGalleryBinding
@@ -30,15 +32,38 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
         binding.apply {
             recyclerView.setHasFixedSize(true)
+            recyclerView.itemAnimator = null // we are not updating any item
             recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
                 header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
                 footer = UnsplashPhotoLoadStateAdapter { adapter.retry() },
             )
+            buttonRetry.setOnClickListener {
+                adapter.retry()
+            }
         }
 
         viewModel.photos.observe(viewLifecycleOwner, Observer {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         })
+
+        adapter.addLoadStateListener { loadState -> //is off type combinedLoadStates in to this object we refresh dataset or we append new data to it
+            binding.apply { //change a few different view
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading // which mean set are we refreshing the list you data set
+                recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading //whick means that loading has finished but it's not an error
+                buttonRetry.isVisible = loadState.source.refresh is LoadState.Error
+                textViewError.isVisible = loadState.source.refresh is LoadState.Error
+
+                //empty view
+                if(loadState.source.refresh is LoadState.NotLoading && //we are not loading right now, but it's also not error because it's empty
+                        loadState.append.endOfPaginationReached && // they aren't anymore results belong to afterword
+                        adapter.itemCount < 1) {
+                    recyclerView.isVisible = false
+                    textViewEmpty.isVisible = true
+                } else {
+                    textViewEmpty.isVisible = false
+                }
+            }
+        }
 
         setHasOptionsMenu(true)
     }
